599af017cd8bd07010115285b2908f4173e375ad
Atisha Shaurya <94087803+Atisha-Shaurya@users.noreply.github.com>
2023-03-09 23:49:21 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,18 @@
+output "core_network_rg_id" {
+
+  value       = var.create_rg ? tostring(azurerm_resource_group.compute_networking_rg[0].id) : ""
+
+  description = "Output Resource Group ID. (Only if new resource group was created as part of this deployment)"
+
+}
+
+
+
+
+output "core_vnet_id" {
+
+  value       = var.create_vnet ? tostring(azurerm_virtual_network.compute_vnet[0].id) : ""
+
+  description = "Output Azure Virtual Network ID. (Only if new Vnet was created as part of this deployment)"
+
+}
